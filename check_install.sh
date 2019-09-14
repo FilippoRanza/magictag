d7a0c19dc6ecc21d748edff587110d209d145ce4
Filippo Ranza <filipporanza@gmail.com>
2019-09-14 18:57:16 +0200

    correct script name

--- check_install.sh
+++ check_install.sh
@@ -8,7 +8,7 @@ BASE_DIR=$(dirname "$FILE")
 echo "$FILE"
 for d in ${PATH//:/ } ; do
     if [[ "$d" ==  "$BASE_DIR" ]] ; then
-        mail.py -h && exit 0
+        magictag.py -h && exit 0
     fi
 done
 
